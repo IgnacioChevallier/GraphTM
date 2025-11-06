f3137ec0725b1812633d3182fdd4ff1ff8e46fa2
IgnacioChevallier <ichevallierboutell@mail.austral.edu.ar>
2025-11-06 13:40:15 +0000

    fix

--- hex-game/tm_explorer.py
+++ hex-game/tm_explorer.py
@@ -11,13 +11,13 @@ Overall arguments, that influence the final outcome of the GraphTM.
 def default_args(**kwargs):
     parser = argparse.ArgumentParser()
     parser.add_argument("--epochs", default=100, type=int) # Total number of times the model will iterate over the entire training dataset
-    parser.add_argument("--number-of-clauses", default=20000, type=int) # Higher number = More complexity in the learned patters
-    parser.add_argument("--T", default=25000, type=int) # Threshold for votes a clause needs
-    parser.add_argument("--s", default=10.0, type=float) # Theshold to include literals
+    parser.add_argument("--number-of-clauses", default=10, type=int) # Higher number = More complexity in the learned patters
+    parser.add_argument("--T", default=100, type=int) # Threshold for votes a clause needs
+    parser.add_argument("--s", default=1.0, type=float) # Theshold to include literals
     parser.add_argument("--number-of-state-bits", default=8, type=int) # Depth 2^8 states
-    parser.add_argument("--depth", default=1, type=int) # Message depth btw. nodes
+    parser.add_argument("--depth", default=2, type=int) # Message depth btw. nodes
     parser.add_argument("--symbols", nargs="+", default=['X', 'O', '.']) #Graph Symbols: X_Player1, O_Player2, ._Empty
-    parser.add_argument("--hypervector-size", default=64, type=int) # Based on the number of symbols
+    parser.add_argument("--hypervector-size", default=32, type=int) # Based on the number of symbols
     parser.add_argument("--hypervector-bits", default=2, type=int) # Bits represent the symbols (2 can represent 4 symbols)
     # Would not change, at least no change in most examples
     parser.add_argument("--message-size", default=256, type=int)
@@ -26,7 +26,7 @@ def default_args(**kwargs):
     parser.add_argument('--one-hot-encoding', dest='one_hot_encoding', default=False, action='store_true')
     
     parser.add_argument("--max-included-literals", default=32, type=int) # Max number of features learned per clause
-    parser.add_argument("--number_of_graphs_train", default=1000, type=int) # Number of graphs used for training
+    parser.add_argument("--number_of_graphs_train", default=10000, type=int) # Number of graphs used for training
     parser.add_argument("--number_of_graphs_test", default=2500, type=int) # Number of graphs used for testing
 
     args = parser.parse_args()
